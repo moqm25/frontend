a4120cda596da1e32d8a8f10ea68de8af9f4eef1
Moiez Qamar <89601171+moqm25@users.noreply.github.com>
2024-02-06 11:37:30 -0500

    additions to customer

---
+++ cs490_front_end/src/CustomerPage.js
@@ -0,0 +1,9 @@
+// CustomerPage.js
+import React from "react";
+import { port } from "./configurations/config.js";
+
+const CustomerPage = () => {
+	return <h1>Customer details will go here {port}</h1>;
+};
+
+export default CustomerPage;
